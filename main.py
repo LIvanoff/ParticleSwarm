ca10938c562152a9d829936b1cde4d26a469db8e
LIvanoff <drfrily@gmail.com>
2022-10-17 19:46:35 +0300

    update test and main

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import numpy as np
 import random
+import os
+from PIL import Image
 
 def particle_swarm():
+    global_min= [] # глобальный оптимум
+    local_min = {} # локальный оптимум всех точек
+    particle_dict = {}
+    velocity = {}
+
+    for x in range(1,21):
+        particle_dict[x] = []
+        particle_dict[x].append([random.randint(0, 100) for i in range(2)])
+        velocity[x] = []
+        velocity[x].append([random.randint(0, 100) for i in range(2)])
+        local_min[x] = []
+        local_min[x] = particle_dict[x]
+
+    image = load_image('./image.gradient.jpg')
+    global_min = search_global_min(image)
+
+    print_particle()
     return
+
+def print_particle():
+    return
+
+def creat_gif():
+    return
+
+def load_image(path : str):
+    with Image.open(path) as img:
+        image = np.asarray(img)
+    return image
+
+def search_global_min(image):
+
+
+    return
+
 
 if __name__ == '__main__':
     particle_swarm()
--- test.py
+++ test.py
@@ -3,12 +3,13 @@ from main import *
 
 class MyTestCase(unittest.TestCase):
     def test_particle_swarm(self):
-        self.assertEqual(particle_swarm(), '\b\w\b'+'.gif')
+        self.assertEqual(particle_swarm(), 'result.gif')
 
     def test_creat_gif(self):
-        self.assertEqual(particle_swarm(), '\b\w\b'+'.gif')
+        self.assertEqual(particle_swarm(), 'result.gif')
+
     def test_print_particle(self):
-        self.assertEqual(particle_swarm(), '\b\w\b'+'.gif')
+        self.assertEqual(particle_swarm(), '\b\w\b'+'.jpg')
 
 
 if __name__ == '__main__':
