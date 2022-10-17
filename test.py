c456de1d950c85d3ca4a13a482c494995e8a43c8
LIvanoff <drfrily@gmail.com>
2022-10-17 16:31:46 +0300

    update test

--- test.py
+++ test.py
@@ -2,8 +2,11 @@ import unittest
 from main import *
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(particle_swarm(), '\b\w\b'+'.png')
+    def test_parcticle_swarm(self):
+        self.assertEqual(particle_swarm(), '\b\w\b'+'.gif')
+
+    def test_creat_gif(self):
+        self.assertEqual(particle_swarm(), '\b\w\b'+'.gif')
 
 
 if __name__ == '__main__':
